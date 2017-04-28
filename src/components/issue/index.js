402b35cc5b2254febc11413641fdd2d3cb01b872
Jesse Sutherland <jesse.a.sutherland@gmail.com>
2017-04-28 16:26:31 -0400

    added some more detail (JSX markup) and Tachyon classes to issue detail view, see #4

--- src/components/issue/index.js
+++ src/components/issue/index.js
@@ -5,14 +5,32 @@ export default class Issue extends Component {
     // issue is from router.
     render ({issue}) {
         return (
-            <div class={style.issue}>
-                <h1> Issue # {issue} </h1>
-                <p class='pa1'>
-                    Hello Joe! I'm an issue!
-                </p>
-            </div>
+            <section class='mt5 pa3 bg-light-gray'>
+                <h1 class='f3'> Issue: {issue.toUpperCase()} </h1>
+                <h2 class='f4'> Status: 
+                    <span class='bg-green dib mh1 ph1 br3 center'>
+                        {'Closed'.toLowerCase()}
+                    </span>
+                    &nbsp; Priority:
+                    <span class='bg-orange dib mh1 ph1 br3 center'>
+                        {'medium'.toLowerCase()}
+                    </span>
+                    &nbsp; Stars:
+                    <span class='bg-yellow dib mh1 ph1 br3 ba bw1 b--white center'>
+                        {5}
+                    </span> 
+                </h2>
+                <div class=''>
+                    <h3 class='f5'> Description: </h3>
+                    <p>
+                        Hello Joe! I'm an issue!
+                    </p>
+                </div>
+            </section>
         );
     };
 }
 
-// TODO: add more load stuff. Presumably use id to load from store? Pouch or whatever.
+// TODO: anything in '{}', e.g 'stars' is a variable that needs to be dynamic.
+
+// TODO: data: add more load stuff. Presumably use id to load from store? Pouch or whatever. This is part of the component lifecycle, i.e. will be on mount or something.
