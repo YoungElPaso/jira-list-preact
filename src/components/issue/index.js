235980b9eeb9d4034bba2d98ca4722f2c3a322d5
Jesse Sutherland <jesse.a.sutherland@gmail.com>
2017-05-06 13:17:50 -0400

    started priority component - changing class on priority value see #7 and see #4

--- src/components/issue/index.js
+++ src/components/issue/index.js
@@ -52,11 +52,25 @@ class Status extends Component {
 // The actual Priority component.
 class Priority extends Component {
 	render({ priority }) {
+    let p = priority.toLowerCase();
+    let classVal = 'bg-orange';
+    if (p == 'lowest') {
+      classVal = 'blue';
+    } else if (p == 'low') {
+      classVal = 'green';
+    } else if (p == 'medium') {
+      classVal = 'orange';
+    } else if (p == 'high') {
+      classVal = 'light-red';
+    } else if (p == 'highest') {
+      classVal = 'red';
+    }
+
 		return (
       <div class="dib mr1">
         Priority:
-        <span class="bg-orange dib mh1 pa1 br3 tc">
-          {priority.toLowerCase()} 🔺
+        <span class={classVal + " dib mh1 pa1 br3 tc"}>
+          {p}
         </span>
       </div>
 		);
